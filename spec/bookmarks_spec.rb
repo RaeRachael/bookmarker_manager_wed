cb4427c4d486e6d12b1164b00a35651627209068
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-06-02 10:51:58 +0100

    merge

--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -3,9 +3,8 @@ require 'bookmarks'
 describe BookmarkList do
 
 subject(:bookmarkList) { described_class.new }
-let(:show) { [ "google", "BBC", "pepperpig" ] }
 
 it "shows the user all of their bookmarks" do
-  expect(bookmarkList.show).to include "google"
+  expect(bookmarkList.all).to include "google"
 end
 end
