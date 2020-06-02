17f6451405911c96708913514e425033c52e3849
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-06-02 15:42:13 +0100

    connect to database

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,12 +3,12 @@ require 'pg'
 class Bookmark
 
   def self.all
-      con = PG.connect :dbname => 'bookmark_manager', :user => 'sophie'
+      con = PG.connect :dbname => 'bookmark_manager'
 
       rs = con.exec "SELECT url FROM bookmarks"
 
-# ["http://www.google.com",
-# "http://www.facebook.com"]
+      rs.map { |bookmark| bookmark['url'] }
+
   end
 
 end
