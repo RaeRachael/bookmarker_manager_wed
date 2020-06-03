798660685327806395794fa7cb6f76b718be115a
Rachael <rachaelewins@hotmail.com>
2020-06-03 16:19:49 +0100

    update to database instructions

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,4 @@
-CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60), title CHAR(20));
+INSERT INTO bookmarks (url, title) VALUES ('http://www.makersacademy.com', 'Makers Academy'),
+ ('http://www.destroyallsoftware.com', 'Destroy All Software'),
+ ('http://www.google.com', 'Google');
